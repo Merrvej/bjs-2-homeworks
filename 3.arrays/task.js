d52a4fd428eb7b00bfc28d8ce3d67c71cc0c45e8
Anastasiia Pavliuk <mervej@yandex.ru>
2023-08-11 09:33:55 +0500

    1 ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        return arr1.every((value, index) => value === arr2[index]);
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    users.filter(element => element.gender === gender).map(element => element.age).reduce((acc, item, index, arr) => {
+        acc+=item;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        } else {
+            return acc;
+        }
+    }, 0)
 }
