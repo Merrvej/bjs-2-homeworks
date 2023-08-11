fe121344d261702fe896bba498cb31ab079043db
Anastasiia Pavliuk <mervej@yandex.ru>
2023-08-11 17:11:49 +0500

    Add files via upload

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    
+    if("marks" in this) {
+        this.marks.push(...marks);
+    } else {
+        console.log("Студент отчислен за" + this.reason + ", поставить ему оценку невозможно");
+    }
 }
 
 Student.prototype.getAverage = function () {
   
+    if("marks" in this && this.marks.length !== 0) {
+        let totalMarkCount = this.marks.reduce((acc, item) => {acc + item}, 0);
+        let averageMark = totalMarkCount / this.marks.length;
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
