a88af4093acaec1962946da8d8d4b5e7faa21596
Anastasiia Pavliuk <mervej@yandex.ru>
2023-08-16 09:48:22 +0500

    final all green

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -44,8 +44,12 @@ function getTriangle(a, b, c) {
       return new Triangle(a, b, c);
     } catch (error) {
       return {
-        area: "Ошибка! Треугольник не существует",
-        perimeter: "Ошибка! Треугольник не существует"
+        get area() {
+            return "Ошибка! Треугольник не существует"
+        },
+        get perimeter() {
+            return "Ошибка! Треугольник не существует"
+        }
       };
     }
   }
