34b4f75d69842882b09ea08e253fd72777291d5d
Anastasiia Pavliuk <mervej@yandex.ru>
2023-08-18 14:44:48 +0500

    task 1 green

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args.join());
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if(objectInCache) {
+        console.log("Из кеша: " + objectInCache.result);
+        return "Из кеша: " + objectInCache.result;
+    }
+    let result = func(...args);
+    cache.push({hash, result});
+    if(cache.length > 5) {
+        cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    function wrapper(...args) {
+        let count = 0;
+        let allCount = 0;
+        let timeoutId = null;
+        if (timeoutId) {
+            count++;
+            allCount++;
+            return;
+        }
+        
+        const result = func(...args);
+        timeoutId = true;
+        setTimeout(() => {
+            timeoutId = false;
+        }, delay);
+        count++;
+        allCount++;
+        return result;
+    }
+    return wrapper;
 }
